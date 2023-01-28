d362951004b8232499103eacb3b0926fc61f4884
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2023-01-29 05:07:36 +0700

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,7 +1,7 @@
 // Owner number
-global.owner = '6285731855426'
+global.owner = '6285887776722'
 // Owner name
-global.owner_name = 'Hans'
+global.owner_name = 'Wildan Izzudin'
 // Database name (Default: database)
 global.database = 'database'
 // Maximum upload file size limit (Default : 100 MB)
@@ -9,7 +9,7 @@ global.max_upload = 100
 // Delay for spamming protection (Default : 3 seconds)
 global.cooldown = 3
 // User Limitation (Default : 25)
-global.limit = 20
+global.limit = 25
 // Time to be temporarily banned and others (Default : 30 minutes)
 global.timer = 1800000
 // Symbols that are excluded when adding a prefix (Don't change it)
@@ -19,32 +19,32 @@ global.blocks = ['91', '92', '94', '94', '212']
 // Put target jid to forward friends story
 global.forwards = global.owner + '@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
-global.Api = new (require('./neoxrApi'))('botzhanapi')
+global.Api = new (require('./neoxrApi'))(process.env.API_KEY)
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
-global.chatai_bid = '168058'
-global.chatai_key = '8uOEiYGNCTwAiE0k'
+global.chatai_bid = '164728'
+global.chatai_key = 'MKPsfkgXLZPGrWoH'
 // Timezone (Default : Asia/Jakarta)
 global.timezone = 'Asia/Jakarta'
 // Bot version
 global.version = '2.2.2',
 // Bot name
-global.botname = `© HanBotz`
+global.botname = `© neoxr-bot v${global.version} (Public Bot)`
 // Footer text
-global.footer = 'https://saweria.co/hanbotz'
+global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ɴᴇᴏxʀ ッ'
 // Global status
 global.status = Object.freeze({
-   wait: Function.texted('bold', 'Processed . . .'),
-   invalid: Function.texted('bold', 'URL is Invalid!'),
-   wrong: Function.texted('bold', 'Wrong format!'),
-   getdata: Function.texted('bold', 'Scraping metadata . . .'),
-   fail: Function.texted('bold', 'Can\'t get metadata!'),
-   error: Function.texted('bold', 'Error occurred!'),
-   errorF: Function.texted('bold', 'Sorry this feature is in error.'),
-   premium: Function.texted('bold', 'This feature only for premium user.'),
-   owner: Function.texted('bold', 'This command only for owner.'),
-   god: Function.texted('bold', 'This command only for Master'),
-   group: Function.texted('bold', 'This command will only work in groups.'),
-   botAdmin: Function.texted('bold', 'This command will work when I become an admin.'),
-   admin: Function.texted('bold', 'This command only for group admin.'),
-   private: Function.texted('bold', 'Use this command in private chat.')
+   wait: Func.texted('bold', 'Processed . . .'),
+   invalid: Func.texted('bold', 'URL is Invalid!'),
+   wrong: Func.texted('bold', 'Wrong format!'),
+   getdata: Func.texted('bold', 'Scraping metadata . . .'),
+   fail: Func.texted('bold', 'Can\'t get metadata!'),
+   error: Func.texted('bold', 'Error occurred!'),
+   errorF: Func.texted('bold', 'Sorry this feature is in error.'),
+   premium: Func.texted('bold', 'This feature only for premium user.'),
+   owner: Func.texted('bold', 'This command only for owner.'),
+   god: Func.texted('bold', 'This command only for Master'),
+   group: Func.texted('bold', 'This command will only work in groups.'),
+   botAdmin: Func.texted('bold', 'This command will work when I become an admin.'),
+   admin: Func.texted('bold', 'This command only for group admin.'),
+   private: Func.texted('bold', 'Use this command in private chat.')
 })
